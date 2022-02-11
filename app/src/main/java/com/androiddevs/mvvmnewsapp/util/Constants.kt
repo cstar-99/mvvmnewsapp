cbbcfa3069c23d270667a117849f78e71e47203a
reddy <reddy1451999@gmail.com>
2022-02-11 17:25:36 +0530

    mvvm news app

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "bba5997ed37d4e60b5202f694ae7a26f"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+    }
+}
